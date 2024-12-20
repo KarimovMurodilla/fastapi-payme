2faac06646adcf3c9c067efcbe80cdf099e06707
KarimovMurodilla <karimovmurodilla15@gmail.com>
2024-12-20 18:00:08 +0500

    initial commit

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+
+
+setup(
+    name="fastapi-payme-lib",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        "fastapi",
+        "uvicorn",
+    ],
+    author="Karimov Murodilla",
+    author_email="karimovmurodilla15@gmail.com",
+    description="A FastAPI library for Payme integration",
+    url="https://github.com/KarimovMurodilla/fastapi-payme.git",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
